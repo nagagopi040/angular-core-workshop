269a72bf7dc1d16e625adcb8cedc455a59a7cf1f
Naga Gopi Raju N <nagagopi401142@gmail.com>
2019-08-31 17:43:33 +0530

    DashBoard Setup with Nx

---
+++ apps/dashboard/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@workshop/material';
+import { HomeModule } from './home/home.module';
+import { CustomersModule } from './customers/customers.module';
+import { ProjectsModule } from './projects/projects.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    BrowserAnimationsModule,
+    MaterialModule,
+    HomeModule,
+    ProjectsModule,
+    CustomersModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
